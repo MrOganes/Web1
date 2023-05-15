2171d5bf4ea6c84785323c709163f53f36812ba6
MrOganes <112868498+MrOganes@users.noreply.github.com>
2023-05-15 23:30:52 +0300

    Add files via upload

---
+++ Задание 7/form.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>WEB 7</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+
+  <body>
+
+    <form action="index.php" method="POST" class="decor">
+      <input type="hidden" name="token" value="<?php
+      echo $token;
+      ?>">
+      <input type="hidden" name="token_time" value="<?php
+      echo $token_time;
+      ?>">
+      <div class="form-left-decoration"></div>
+      <div class="form-right-decoration"></div>
+      <div class="circle"></div>
+      <div class="form-inner">
+        <h3 style="text-align: center;">Форма для отправки данных</h3>
+      <div style="color: red; margin-bottom: 23px;  text-align: left; font-size: 16px; <?php
+      if(empty($error)){
+        echo "display: none;";
+      }
+      ?>">
+      <ul>
+      <?php
+      foreach($error as $cout){
+        echo "<li>$cout</li>";
+      }
+      ?>
+      </ul>
+      </div>
+
+      <div style="color: green; height: 25px; margin-bottom: 23px; text-align: center; font-size: 16px; <?php
+      if(empty($rez)){
+        echo "display: none;";
+      }
+      ?>">
+      <?php
+      if($rez==1){
+        echo "Ваши данные успешно отправлены.";
+      }
+      else{
+        echo "Ошибка отпраки данных. Попробуйте ещё раз.";
+      }
+      ?>
+      </div>
+
+        <input type="text" placeholder="Введите имя" name="name" style="
+        <?php
+          if (!empty($error[0])) {
+            echo "background: #ffbaba";
+          }
+        ?>
+        "
+        value="<?php
+          if(!empty($name)){
+            echo "$name";
+          }
+        ?>"/>
+        
+        <input type="email" placeholder="Email" name="email" style="
+        <?php
+          if (!empty($error[1])) {
+            echo "background: #ffbaba";
+          }
+        ?>
+        "
+        value="<?php
+          if(!empty($email)){
+            echo "$email";
+          }
+        ?>"/>
+        <a style="padding-left: 5px" style="white-space: nowrap"
+          >Укажите год вашего рождения:
+          <select
+            id="year"
+            name="year"
+            size="1"
+            style="display: inline"
+          ></select
+        ></a>
+        <script>
+          var objSel = document.getElementById("year");
+          var c = 0;
+          for (var i = 2023; i >= 1920; i--) {
+            objSel.options[c] = new Option(i, i);
+            c++;
+          }
+          document.getElementById('year').querySelectorAll('option')[2023-Number(<?php
+          if(!empty($year)){
+            echo $year;
+          }
+          ?>)].selected = true;;
+        </script>
+        <span style="<?php
+        if(!empty($error[3])){
+          echo "background: #ffbaba; border-radius: 20px;";
+        }
+        ?>"><a style="margin-left: 5px">Укажите пол: </a
+        ><a style="margin-left: 15px"
+          >Мужской<input
+            type="radio"
+            name="pol"
+            class="radio"
+            value="Мужской" 
+            <?php
+            if($gender=="Мужской"){
+              echo "checked";
+            }
+            ?>/></a
+        ><a style="margin-left: 10px"
+          >Женский<input type="radio" name="pol" class="radio" value="Женский"
+          <?php
+            if($gender=="Женский"){
+              echo "checked";
+            }
+          ?>/></a></span>
+        <br />
+        <span style="<?php
+        if(!empty($error[4])){
+          echo "background: #ffbaba; border-radius: 20px;";
+        }
+        ?>"><a style="margin-left: 5px"
+          >Кол-во конечностей:
+          <a style="margin-left: 15px"
+            >2 <input type="radio" name="kol" value="2"
+            <?php
+            if($kol=="2"){
+              echo "checked";
+            }
+            ?>
+          /></a>
+          <a style="margin-left: 18px"
+            >4 <input type="radio" name="kol" value="4"
+            <?php
+            if($kol=="4"){
+              echo "checked";
+            }
+            ?>
+          /></a>
+          <a style="margin-left: 18px"
+            >6 <input type="radio" name="kol" value="6"
+            <?php
+            if($kol=="6"){
+              echo "checked";
+            }
+            ?>
+             style="margin-bottom: 30px"
+            />
+          </a>
+        </a>
+          </span>
+        <a style="margin-left: 6px">
+          Укажите сверхспособности:
+          <br />
+          <select
+            name="superpowers[]"
+            style="
+              display: inline;
+              width: 180px;
+              overflow-y: hidden;
+              height: 60px;
+              margin-top: 10px;
+              margin-bottom: 30px;
+              <?php
+              if(!empty($error[5])){
+                echo "background: #ffbaba;";
+              }
+              ?>
+            "
+            multiple="multiple"
+          >
+            <option value="бессмертие" <?php
+                if($immortality=="yes"){
+                  echo "selected";
+                }
+            ?>>бессмертие</option>
+            <option value="прохождение сквозь стены" <?php
+                if($passing_through_walls=="yes"){
+                  echo "selected";
+                }
+            ?>>
+              прохождение сквозь стены
+            </option>
+            <option value="левитация" <?php
+                if($levitation=="yes"){
+                  echo "selected";
+                }
+            ?>>левитация</option>
+          </select>
+        </a>
+        <textarea placeholder="Биография" rows="3" name="biography" maxlength="255" style="<?php
+        if(!empty($error[6])){
+          echo "background: #ffbaba; border-radius: 20px;";
+        }
+        ?>"><?php
+        if(!empty($biography)){
+          echo $biography;
+        }
+        ?></textarea>
+        <a style="display: flex; vertical-align: middle"
+          ><input
+            type="checkbox"
+            style="width: 18px; height: 18px; margin-right: 10px"
+            name="ok"
+            value="Согласен"
+          /><span style="margin-top: 3px"
+            >C <span style="color: red">правилами</span> ознакомлен.</span
+          ></a
+        >
+        <div
+          style="
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            height: 50px;
+          "
+        >
+          <input type="submit" value="Отправить" style="margin: 15px" />
+        </div>
+      </div>
+    </form>
+  </body>
+</html>
---
+++ Задание 7/index.php
@@ -0,0 +1,274 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+$name = $email = $year = $gender = $kol  = $biography = $ok = "";
+$immortality='no';
+$passing_through_walls='no';
+$levitation='no';
+$rez="";
+
+$error = [];
+
+function Test_name($data){
+  $pattern = '/^[а-яёЁА-Я]+$/u';
+  if(preg_match($pattern, $data)){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
+
+function Test_email($data){
+  $pattern = '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/';
+  if(preg_match($pattern, $data)){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
+
+function Test_year($data){
+  $pattern = '/^[1-9][0-9]*$/';
+  if(preg_match($pattern, $data)){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
+
+function Test_gender($data){
+  if($data=="Мужской" || $data="Женский"){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
+
+function Test_limbs($data){
+  if($data="2" || $data="4" || $data="6"){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
+
+function Test_superpowers($data){
+  foreach($data as $cout){
+    if($cout!="бессмертие" && $cout!="прохождение сквозь стены" && $cout!="левитация"){
+      return 0;
+    }
+  }
+  return 1;
+}
+
+function Test_biography($data){
+  $pattern = '/^[a-zA-Zа-яА-Я\s.,!?-]{1,255}$/u';
+  if(preg_match($pattern, $data)){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
+
+
+if($_SERVER["REQUEST_METHOD"] == "GET"){
+    $token = bin2hex(random_bytes(32));
+    $token_time=time();
+    $user = 'u52879';
+    $pass = '2750849';
+    $conn = new PDO('mysql:host=localhost;dbname=u52879', $user, $pass, [PDO::ATTR_PERSISTENT => true]);
+    $stmt = $conn->prepare("REPLACE INTO TOKENS(token, token_time) VALUES (:token, :token_time)");
+    $rez_time=$stmt->execute(['token'=>"$token",'token_time'=>"$token_time"]);
+    include(dirname(__FILE__) . '/form.php');
+    exit();
+}
+else{
+    $token = $_POST['token'];
+    $token_time = $_POST["token_time"];
+    $user = 'u52879';
+    $pass = '2750849';
+    $conn = new PDO('mysql:host=localhost;dbname=u52879', $user, $pass, [PDO::ATTR_PERSISTENT => true]);
+    $stmt = $conn->prepare("SELECT * FROM TOKENS where token=:token");
+    $stmt->execute(['token'=>"$token"]);
+    $result_token = $stmt->fetchAll();
+    if($result_token[0]['token']!=$token || $result_token[0]['token_time']-time()>60*60){
+        echo "Что-то пошло не так. Попробуйте ещё раз.";
+        exit();
+    }
+    else{
+      if (empty($_POST["name"])) {
+        $error[0] = "Введите имя!";
+      } 
+      else {
+        $name = $_POST["name"];
+        if(!Test_name($_POST["name"])){
+          $error[0]="Для записи имени разрешены символы а-я.";
+        }
+        else{
+          unset($error[0]);
+        }
+      }
+
+
+      if (empty($_POST["email"])) {
+        $error[1] = "Введите email!";
+      } 
+      else {
+        $email = $_POST["email"];
+        if(!Test_email($_POST["email"])){
+          $error[1]="Недопустимый адрес email.";
+        }
+        else{
+          unset($error[1]);
+        }
+      }
+
+
+      if(empty($_POST["year"])){
+        $error[2]="Введите год рождения!";
+      }
+      else{
+        $year = $_POST["year"];
+        if(!Test_year($_POST["year"])){
+          $error[2]="Недопустимое значение в поле год рождения.";
+        }
+        else{
+          unset($error[2]);
+        }
+      }
+
+    
+      if(empty($_POST["pol"])){
+        $error[3]="Укажите пол!";
+      }
+      else{
+        $gender = $_POST["pol"];
+        if(!Test_gender($_POST["pol"])){
+          $error[3]="Недопустимое значение при выбора пола.";
+        }
+        else{
+          unset($error[3]);
+        }
+      }
+
+    
+      if(empty($_POST["kol"])){
+        $error[4]="Укажите кол-во конечностей!";
+      }
+      else{
+        $kol = $_POST["kol"];
+        if(!Test_limbs($_POST["kol"])){
+          $error[4]="Недопустимое значение при выбора кол-ва конечностей.";
+        }
+        else{
+          unset($error[4]);
+        }
+      }
+
+    
+      if(empty($_POST["superpowers"])){
+        $error[5]="Укажите сверхспособности!";
+      }
+      else{
+        $superpowers = $_POST["superpowers"];
+        foreach($superpowers as $cout){
+          if($cout =="бессмертие"){
+            $immortality="yes";
+          }
+          if($cout =="прохождение сквозь стены"){
+            $passing_through_walls="yes";
+          }
+          if($cout =="левитация"){
+            $levitation="yes";
+          }
+        }
+        if(!Test_superpowers($_POST["superpowers"])){
+          $error[5]="Недопустимое значение при выбора сверхспособностей!";
+        }
+        else{
+          unset($error[5]);
+        }
+      }
+    
+      if(empty($_POST["biography"])){
+        $error[6]="Заполните биографию!";
+      }
+      else{
+        $biography = $_POST["biography"];
+        if(!Test_biography($_POST["biography"])){
+          $error[6]="В поле биография доступны только символы кириллицы, латиницы и знаки препинания.";
+        }
+        else{
+          unset($error[6]);
+        }
+      }
+
+    
+      if(empty($_POST["ok"])){
+        $error[7]="Примите правила компании!";
+      }
+      else{
+        unset($error[7]);
+      }
+
+      
+      if(empty($error)){
+        $flag = 0;
+        $user = 'u52879';
+        $pass = '2750849';
+        $conn = new PDO('mysql:host=localhost;dbname=u52879', $user, $pass, [PDO::ATTR_PERSISTENT => true]);
+        $stmt = $conn->prepare("INSERT INTO FORMS(name, email, year, gender, limbs, biography) VALUES (:name, :email, :year, :gender, :limbs, :biography)");
+        $rez=$stmt->execute(['name'=>"$name",'email'=>"$email", 'year'=>"$year", 'gender'=>"$gender", 'limbs'=>"$kol", 'biography'=>"$biography"]);
+        $id_form=$conn->lastInsertId();
+        if($rez != 1){
+          $flag+=1;
+        }
+
+
+        if($immortality=="yes"){
+          $num = 1;
+          $stmt2=$conn->prepare("INSERT INTO FORMS_SUPERPOWERS(id, id_superpower) VALUES (:id, :id_superpower)");
+          $rez2=$stmt2->execute(['id'=>"$id_form", 'id_superpower'=>"$num"]);
+          if($rez2 != 1){
+            $flag+=1;
+          }
+        }
+
+        if($passing_through_walls=="yes"){
+          $num = 2;
+          $stmt3=$conn->prepare("INSERT INTO FORMS_SUPERPOWERS(id, id_superpower) VALUES (:id, :id_superpower)");
+          $rez3=$stmt3->execute(['id'=>"$id_form", 'id_superpower'=>"$num"]);
+          if($rez3 != 1){
+            $flag+=1;
+          }
+        }
+        if($levitation=="yes"){
+          $num = 3;
+          $stmt4=$conn->prepare("INSERT INTO FORMS_SUPERPOWERS(id, id_superpower) VALUES (:id, :id_superpower)");
+          $rez4=$stmt4->execute(['id'=>"$id_form", 'id_superpower'=>"$num"]);
+          if($rez4 != 1){
+            $flag+=1;
+          }
+        }
+        
+        if($flag==0){
+          $rez=1;
+        }
+        else{
+          $rez=0;
+        }
+
+        $name = $email = $year = $gender = $kol  = $biography = $ok = "";
+        $immortality='no';
+        $passing_through_walls='no';
+        $levitation='no';
+      }
+      include(dirname(__FILE__) . '/form.php');
+    }
+}
+?>
